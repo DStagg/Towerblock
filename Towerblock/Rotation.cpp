921b1bbc0d4788d31e78db5a90c6c8a54fa8eea8
DStagg <staggy11@gmail.com>
2018-04-02 12:49:45 +0100

    Fixed CalcDistance().

--- Towerblock/Rotation.cpp
+++ Towerblock/Rotation.cpp
@@ -50,7 +50,7 @@ float CalcYComp(float heading)
 
 float CalcDistance(float x1, float y1, float x2, float y2)
 {
-	float dx = x2 - x2;
+	float dx = x2 - x1;
 	float dy = y2 - y1;
 	return sqrtf((dx * dx) + (dy * dy));
 };
