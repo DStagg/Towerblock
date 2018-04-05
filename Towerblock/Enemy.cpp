480af0703d5df71cb631b1ee0411af8bff2921e7
DStagg <staggy11@gmail.com>
2018-04-05 21:14:35 +0100

    Show enemy facing.

--- Towerblock/Enemy.cpp
+++ Towerblock/Enemy.cpp
@@ -19,7 +19,7 @@ void Enemy::Update(float dt)
 {
 	Entity::Update(dt);
 
-	//_Mask._Mask = Circle(_Position.GetX(), _Position.GetY(), 16);
+	_Facing = CalcHeading(_Velocity._X, _Velocity._Y);
 };
 
 void Enemy::Draw(sf::RenderWindow* rw)
@@ -30,6 +30,14 @@ void Enemy::Draw(sf::RenderWindow* rw)
 	circ.setPosition((float)_Position.GetX(), (float)_Position.GetY());
 	circ.setOrigin(circ.getRadius(), circ.getRadius());
 	rw->draw(circ);
+
+	//	Indicate facing
+	sf::CircleShape circ2;
+	circ2.setRadius(2.5f);
+	circ2.setFillColor(sf::Color::White);
+	circ2.setOrigin(2.5f, 2.5f);
+	circ2.setPosition(_Position.GetX() + (32.f * CalcXComp(_Facing)), _Position.GetY() + (32.f * CalcYComp(_Facing)));
+	rw->draw(circ2);
 };
 
 CircleMask Enemy::GetMask()
