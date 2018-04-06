2968ba1f9618ea02c28ce66ffa785544077526f3
DStagg <staggy11@gmail.com>
2018-04-06 10:36:34 +0100

    Added ammo tracking to guns.

--- Towerblock/Gun.h
+++ Towerblock/Gun.h
@@ -15,6 +15,8 @@ public:
 	virtual std::vector<Bullet> Fire(int fpx, int fpy, float facing) = 0;
 
 	float _Cooldown;
+	int _MaxAmmo;
+	int _CurrentAmmo;
 };
 
 class Pistol : public Gun
@@ -24,17 +26,24 @@ public:
 	Pistol()
 	{
 		_Cooldown = 0.2f;
+		_MaxAmmo = 20;
+		_CurrentAmmo = 20;
 	};
 
 	std::vector<Bullet> Fire(int fpx, int fpy, float facing)
 	{
 		std::vector<Bullet> results;
 		
-		Bullet b1;
-		b1._Position.Set(fpx, fpy);
-		b1._Velocity = Vec(CalcXComp(facing), CalcYComp(facing)).UnitVec() * 100.f;
-		results.push_back(b1);
-		
+		if (_CurrentAmmo > 0)
+		{
+			_CurrentAmmo -= 1;
+
+			Bullet b1;
+			b1._Position.Set(fpx, fpy);
+			b1._Velocity = Vec(CalcXComp(facing), CalcYComp(facing)).UnitVec() * 100.f;
+			results.push_back(b1);
+		}
+
 		return results;
 	};
 };
@@ -46,30 +55,37 @@ public:
 	Shotgun()
 	{
 		_Cooldown = 0.5f;
+		_MaxAmmo = 5;
+		_CurrentAmmo = 5;
 	};
 
 	std::vector<Bullet> Fire(int fpx, int fpy, float facing)
 	{
 		std::vector<Bullet> results;
 
-		Bullet temp;
-		temp._Position.Set(fpx, fpy);
-		temp._Velocity = Vec(CalcXComp(facing), CalcYComp(facing)).UnitVec() * 100.f;
-		results.push_back(temp);
+		if (_CurrentAmmo > 0)
+		{
+			_CurrentAmmo -= 1;
 
-		float spread = DegreeToRad(45.f);
+			Bullet temp;
+			temp._Position.Set(fpx, fpy);
+			temp._Velocity = Vec(CalcXComp(facing), CalcYComp(facing)).UnitVec() * 100.f;
+			results.push_back(temp);
 
-		temp._Velocity = Vec(CalcXComp(facing - spread), CalcYComp(facing - spread)).UnitVec() * 100.f;
-		results.push_back(temp);
+			float spread = DegreeToRad(45.f);
 
-		temp._Velocity = Vec(CalcXComp(facing + spread), CalcYComp(facing + spread)).UnitVec() * 100.f;
-		results.push_back(temp);
-		
-		temp._Velocity = Vec(CalcXComp(facing - (spread / 2.f)), CalcYComp(facing - (spread / 2.f))).UnitVec() * 100.f;
-		results.push_back(temp);
-		
-		temp._Velocity = Vec(CalcXComp(facing + (spread / 2.f)), CalcYComp(facing + (spread / 2.f))).UnitVec() * 100.f;
-		results.push_back(temp);
+			temp._Velocity = Vec(CalcXComp(facing - spread), CalcYComp(facing - spread)).UnitVec() * 100.f;
+			results.push_back(temp);
+
+			temp._Velocity = Vec(CalcXComp(facing + spread), CalcYComp(facing + spread)).UnitVec() * 100.f;
+			results.push_back(temp);
+
+			temp._Velocity = Vec(CalcXComp(facing - (spread / 2.f)), CalcYComp(facing - (spread / 2.f))).UnitVec() * 100.f;
+			results.push_back(temp);
+
+			temp._Velocity = Vec(CalcXComp(facing + (spread / 2.f)), CalcYComp(facing + (spread / 2.f))).UnitVec() * 100.f;
+			results.push_back(temp);
+		}
 
 		return results;
 	};
