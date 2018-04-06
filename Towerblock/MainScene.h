bf6862dc7b1604881f8fcb82c97cad295a9c7881
DStagg <staggy11@gmail.com>
2018-04-06 11:32:14 +0100

    Base mvp done - now to add SFX, sprites and JUICE.

--- Towerblock/MainScene.h
+++ Towerblock/MainScene.h
@@ -49,7 +49,6 @@ private:
 	Level _Level;
 };
 
-//	TODO: 7.4 Ammo can be collected
 //	TODO: 8.1 SFX for each fire mode
 //	TODO: 8.2 SFX for player harm & death
 //	TODO: 8.3 SFX for enemy harm & death
