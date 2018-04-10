df52e5a54634021e407fc7af2507cd6320bfc102
DStagg <staggy11@gmail.com>
2018-04-10 19:52:23 +0100

    Clean revert before trying to get a common interface for BinaryFile and TextFile working.

---
+++ Towerblock/Framework/IO/BaseFile.h
@@ -0,0 +1,29 @@
+#ifndef BASE_FILE_H
+#define BASE_FILE_H
+
+class BaseFile
+{
+public:
+
+	//	Read
+	//	Write
+	//	Open
+	//	Close
+	//	IsOpen
+
+	//	Get/Set Filename
+
+protected:
+
+	//	Filename
+	//	Mode (In/Out)
+	//	Overwrite/Append
+
+private:
+
+
+
+};
+
+
+#endif
